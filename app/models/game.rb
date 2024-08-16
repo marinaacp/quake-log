a2be76ee0c1243a6b6bac9f77a3ea2e62e31d25b
marina pessoa <marinacp.pessoa@gmail.com>
2024-08-16 15:29:26 -0300

    model: added enums to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,15 @@
 class Game < ApplicationRecord
   has_many :players, dependent: :destroy
   has_many :kills, through: :players
+
+  enum gametype: {
+    gt_ffa: 0,
+    gt_tournament: 1,
+    gt_single_player: 2,
+    gt_team: 3,
+    gt_ctf: 4,
+    gt_1fctf: 5,
+    gt_obelisk: 6,
+    gt_harvester: 7     
+  }
 end
--- app/models/kill.rb
+++ app/models/kill.rb
@@ -1,4 +1,31 @@
 class Kill < ApplicationRecord
   belongs_to :killer, class_name: 'Player', optional: true
   belongs_to :victim, class_name: 'Player'
+
+  enum type_death: {
+    mod_unknown: 0,
+    mod_shotgun: 1,
+    mod_gauntlet: 2,
+    mod_machinegun: 3,
+    mod_grenade: 4,
+    mod_grenade_splash: 5,
+    mod_rocket: 6,
+    mod_rocket_splash: 7,
+    mod_plasma: 8,
+    mod_plasma_splash: 9,
+    mod_railgun: 10,
+    mod_lightning: 11,
+    mod_bfg: 12,
+    mod_bfg_splash: 13,
+    mod_water: 14,
+    mod_slime: 15,
+    mod_lava: 16,
+    mod_crush: 17,
+    mod_telefrag: 18,
+    mod_falling: 19,
+    mod_suicide: 20,
+    mod_target_laser: 21,
+    mod_trigger_hurt: 22,
+    mod_grapple: 23
+  } # This enum does not include MISSIONPACK games
 end
